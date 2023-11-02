b1e6e2040e089e35aa388ff88d95014cf0dc7910
Catian7 <149692429+Catian7@users.noreply.github.com>
2023-11-02 18:51:00 +0300

    Wrote prime-number search function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+static void showPrimeNums(int upperLimit) {
+    int count = 0;
+    for (int i = 1; i <= upperLimit; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                count++;
+            }
+        }
+        if (count == 2) 
+        {
+            System.Console.WriteLine(i);
+        }
+        count = 0;
+    }
+}
